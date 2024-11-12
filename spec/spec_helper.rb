3a8e772be296794822eac7f8680f142a9aab6ad5
Rodrigo Lima Jaroszewski <rodrigo.lima@avidity.se>
2024-11-12 07:55:15 -0300

    Gemfile and RSpec setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+# Add lib directory to load path
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+RSpec.configure do |config|
+  # Basic configuration
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
